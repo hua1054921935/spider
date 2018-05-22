e96b0b49d844facd4190758c61e541a301d9f5eb
xwp <xwp_fullstack@163.com>
2018-05-22 09:15:09 +0800

    完成3

--- book.py
+++ book.py
@@ -1,15 +1,17 @@
 # # coding=utf-8
 import requests
 from lxml import etree
+from selenium import webdriver
 
 class Book():
-    def __init__(self):
+    def __init__(self, name):
 
-        self.url = 'http://www.cuiweijuxing.com/files/article/html/0/92/index.html'
+        self.name = name
         self.headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64)' 'AppleWebKit/537.36 (KHTML, like Gecko)' 'Chrome/63.0.3239.84 Safari/537.36','X-CSRFToken':'9303a1f4b25e8f2f68d89b1d0e9cf615','Referer':'http://neihanshequ.com/bar/1/','Cookie':'uuid="w:99d7c4dc60eb4ae09556c6bc9dc6d253"; tt_webid=6529659478042854919; csrftoken=13753cb398583c33da5be8bf0f7727f7; _ga=GA1.2.1382869523.1520304821; _gid=GA1.2.1200588661.1520422263'}
         # self.name='《'+name+'》正文'
-    def parse_url(self):
-        response = requests.get(self.url, headers=self.headers)
+
+    def parse_url(self, url):
+        response = requests.get(url, headers=self.headers)
         html = response.content.decode('gbk')
         return html
 
@@ -17,7 +19,7 @@ class Book():
         data = etree.HTML(html)
         book_name = data.xpath('//div[@style="text-align:center;"]/span[@style="font-size:20px;font-weight:bold;color:#f27622;"]/text()')
         # chapter_list = data.xpath('//li[@class="chapter"]')
-        chapter_list = data.xpath('//div[text()="《明星潜规则之皇》正文"]/parent::div//li[@class="chapter"]')
+        chapter_list = data.xpath('//div[text()="《' + self.name + '》正文"]/parent::div//li[@class="chapter"]')
         # chapter_list.reverse()
         for chapter in chapter_list:
             href = chapter.xpath('./a/@href')[0]
@@ -30,17 +32,34 @@ class Book():
                 print('写入' + chapter_name + '完成')
 
     def get_chapter_content(self, url):
-        response = requests.get(url, headers=self.headers)
+        response = requests.get(url=url, headers=self.headers)
         html = response.content.decode('gbk', errors='ignore')
         data = etree.HTML(html)
         content = data.xpath('//div[@class="content"]/text()')
         return content
 
+    def search_book(self, name):
+        drive = webdriver.Chrome()
+        drive.get('http://www.cuiweijuxing.com/modules/article/search.php')
+        search = drive.find_element_by_name('searchkey')
+        search.send_keys(name)
+        submit = drive.find_element_by_name('submit')
+        submit.click()
+        # a=drive.find_element_by_class_name('c_subject')
+        url = drive.find_element_by_xpath('//span[@class="c_subject"]/a').get_attribute("href")
+        drive.close()
+        return url
+
+
+
     def run(self):
-        html=self.parse_url()
+        url = self.search_book(self.name)
+        print(url)
+        html = self.parse_url(url)
         self.get_chapter(html)
 if __name__ == '__main__':
-    spider=Book()
+    name = input('请输入书名：')
+    spider = Book(name)
     spider.run()
 #
 #
--- text.py
+++ text.py
@@ -1,6 +1,12 @@
-x="第1章 战龙龙珠"
-if "第1章" in x:
-    print(666)
+from selenium import webdriver
 
-a=['1','2','3']
-print(str(a))
+drive = webdriver.Chrome()
+drive.get('http://www.cuiweijuxing.com/modules/article/search.php')
+search = drive.find_element_by_name('searchkey')
+search.send_keys('白洁')
+submit = drive.find_element_by_name('submit')
+submit.click()
+# a=drive.find_element_by_class_name('c_subject')
+url = drive.find_element_by_xpath('//span[@class="c_subject"]/a').get_attribute("href")
+
+drive.close()
